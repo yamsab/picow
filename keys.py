7561e81dfa7b5406d14e52ad94148b5603973b13
Yama Sabit <ys222fh@student.lnu.se>
2024-06-19 18:43:36 +0200

    testing wifi

---
+++ keys.py
@@ -0,0 +1,2 @@
+WIFI_SSID = 'Kabul_2G_Guest'
+WIFI_PASS = 'Sabit786'
---
+++ main.py
@@ -0,0 +1,24 @@
+import machine
+import time
+
+adc = machine.ADC(27)
+sf = 4095/65535 # Scale factor
+volt_per_adc = (3.3 / 4095)
+
+"""while True: 
+    time.sleep(2)
+    millivolts = adc.read_u16()
+
+    adc_12b = millivolts * sf
+
+    volt = adc_12b * volt_per_adc
+
+    # MCP9700 characteristics
+    dx = abs(50 - 0)
+    dy = abs(0 - 0.5)
+
+    shift = volt - 0.5
+
+    temp = shift / (dy / dx)
+    print(temp)
+    time.sleep(1)"""
